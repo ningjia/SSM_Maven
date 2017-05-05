669108702c8bd94267075b5eba7645b6bc00795c
neil.jia <neil.jia@elitesland.com>
2017-05-05 23:06:22 +0800

    test

--- readme/buildTable.sql
+++ readme/buildTable.sql
@@ -8,3 +8,21 @@ CREATE TABLE `myTest`.`user` (
   `address` varchar(256) DEFAULT NULL COMMENT '地址',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `myTest`.`contactInfo`;
+CREATE TABLE `contactInfo` (
+  `conId` int(11) NOT NULL,
+  `studId` int(11) DEFAULT NULL,
+  `address` varchar(45) DEFAULT NULL,
+  `zip` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`conId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `myTest`.`course`;
+CREATE TABLE `course` (
+  `courseId` int(11) NOT NULL,
+  `studId` int(11) DEFAULT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `desc` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`courseId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
